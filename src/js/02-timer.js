0b085457487998c4ae290d5d68e8d9a922a194d9
Nazar Smutnitskiy <nsmutnitskiy@swivl.com>
2024-02-17 13:02:48 +0200

    add timer subtask

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,24 +1,85 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const options = {
+const DEFAULT_TIME = '00';
+
+const startBtn = document.querySelector('[data-start]');
+const dateTime = document.getElementById('datetime-picker');
+
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+startBtn.disabled = true;
+
+const onClose = (selectedDates) => {
+  const selectedTime = selectedDates[0].getTime();
+  const currentTime = new Date().getTime();
+
+  if (currentTime > selectedTime) {
+    window.alert('Please choose a date in the future')
+    return;
+  }
+
+  startBtn.disabled = false;
+  finishedAt = selectedTime;
+}
+
+flatpickr("#datetime-picker", {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+  onClose,
+});
 
+let finishedAt = 0;
+let timerId = null;
 
-// const datePicker = document.querySelector('input[type="text"]');
-const datePicker = document.querySelector('#datetime-picker');
-// const datePicker = document.querySelector('input#datetime-picker');
+const onStart = () => {
+  dateTime.disabled = true;
+  startBtn.disabled = true;
 
-flatpickr(datePicker, options)
+  timerId = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const interval = finishedAt - currentTime;
 
-function convertMs(ms) {
+    if (0 > interval) {
+      clearInterval(timerId);
+
+      dateTime.disabled = false;
+
+      days.textContent = DEFAULT_TIME;
+      hours.textContent = DEFAULT_TIME;
+      minutes.textContent = DEFAULT_TIME;
+      seconds.textContent = DEFAULT_TIME;
+
+      return;
+    }
+
+    const {
+      days: daysValue,
+      hours: hoursValue,
+      minutes: minutesValue,
+      seconds: secondsValue,
+    } = convertMs(interval);
+
+    days.textContent = padNumberWithZero(daysValue);
+    hours.textContent = padNumberWithZero(hoursValue);
+    minutes.textContent = padNumberWithZero(minutesValue);
+    seconds.textContent = padNumberWithZero(secondsValue);
+  }, 1000);
+}
+
+const padNumberWithZero = (value) => {
+  if (typeof value !== "number" || isNaN(value)) {
+    return "00";
+  }
+  return value.toString().padStart(2, "0");
+}
+
+const convertMs = (ms) => {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -31,3 +92,5 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+startBtn.addEventListener('click', onStart);
